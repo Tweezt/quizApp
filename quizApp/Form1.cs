3a4a8e2ee136cc3a647eaa373632c00f07ee9cc3
Tweezt <tweezt@gmail.com>
2019-05-30 23:29:04 +0200

    json parsing

--- quizApp/Form1.cs
+++ quizApp/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace quizApp
 {
@@ -17,8 +19,23 @@ namespace quizApp
             InitializeComponent();
         }
 
-        private string path;
-        private void button1_Click(object sender, EventArgs e)
+        public class Answer
+        {
+            public string Text { get; set; }
+            public int AnswerKind { get; set; }
+            public int Points { get; set; }
+        }
+
+        public class RootObject
+        {
+            public string Text { get; set; }
+            public List<Answer> Answers { get; set; }
+        }
+
+
+        public string path;
+        List<RootObject> items;
+        public void button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog file = new OpenFileDialog();
             if (file.ShowDialog() == DialogResult.OK)
@@ -26,6 +43,22 @@ namespace quizApp
                 path = file.FileName;
             }
             textBox1.Text = path;
+
+            using (StreamReader r = new StreamReader(path))
+            {
+                string json = r.ReadToEnd();
+                items = JsonConvert.DeserializeObject<List<RootObject>>(json);
+            }
+
+            foreach (var pytanie in items)
+            {
+                Console.WriteLine(pytanie.Text);
+                foreach(var odpowiedz in pytanie.Answers)
+                {
+                    Console.WriteLine("Punkty: {0}, waga: {1}",odpowiedz.AnswerKind.ToString(),odpowiedz.Points);
+
+                }
+            }
         }
     }
 }
